fed84f503487b5fe4c8713febb4d3169c1c59851
Martin <martinrodriguez@velocitypartners.net>
2016-03-22 11:53:09 -0300

    add homework project

---
+++ HomeWork/HomeWork.Test/MathUtilsTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework.Test
+{
+    [TestClass]
+    public class MathUtilsTest
+    {
+        [TestMethod]
+        public void Raiz9Es3()
+        {
+            Assert.AreEqual(3, MathUtils.RaizCuadrada(9));
+        }
+    }
+}
---
+++ HomeWork/HomeWork/MathUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework
+{
+    public class MathUtils
+    {
+        public static decimal RaizCuadrada(int numero)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
