bd21d99fe1a4486a1a0ac5626a77210f3b9dd4e4
Martin <martinrodriguez@velocitypartners.net>
2016-03-23 15:58:53 -0300

    removing example implementation from test

--- Kata.FizzBuzz.Implementation/Kata.FizzBuzz.Test/FizzBuzzTransformerTest.cs
+++ Kata.FizzBuzz.Implementation/Kata.FizzBuzz.Test/FizzBuzzTransformerTest.cs
@@ -16,7 +16,7 @@
         [TestInitialize]
         public void Setup()
         {
-            this._transformer = new FizzBuzzTransformerExample();
+            this._transformer = new FizzBuzzTransformer();
         }
 
         [TestMethod]
